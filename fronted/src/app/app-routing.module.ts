a29cde2195e4c8c69a97abffc9d571963f6bb25a
shani-t <shanitoledano1@gmail.com>
2023-01-20 02:22:09 +0200

    Update app-routing.module.ts

--- fronted/src/app/app-routing.module.ts
+++ fronted/src/app/app-routing.module.ts
@@ -1,20 +1,16 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
 const appRoutes: Routes = [
   { path: '', redirectTo: '/characters', pathMatch: 'full' },
   { path: 'characters', 
   loadChildren: () => import('./pages/characters/characters.module').then(m => m.CharactersModule),
   },
-  // {
-  //   path: '**',
-  //   redirectTo: '/page-not-found',
-  // },
 ];
 
 @NgModule({
   imports: [
-    RouterModule.forRoot(appRoutes/*, { preloadingStrategy: PreloadAllModules }*/)
+    RouterModule.forRoot(appRoutes)
   ],
   exports: [RouterModule]
 })
