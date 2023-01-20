6e2b3e2d2f49cfed76e83909cf81f2ea833cb662
shani-t <shanitoledano1@gmail.com>
2023-01-20 02:20:17 +0200

    Update app.component.ts

--- fronted/src/app/app.component.ts
+++ fronted/src/app/app.component.ts
@@ -1,8 +1,4 @@
-import { DOCUMENT } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
-import { Component, Inject, OnInit, Renderer2 } from '@angular/core';
-import { Store } from '@ngxs/store';
-import { Observable } from 'rxjs';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -11,19 +7,9 @@ import { Observable } from 'rxjs';
 })
 export class AppComponent implements OnInit {
   title = 'SQREAM_TASK_SHAMI';
-  isLoggedIn$!: Observable<boolean>;
-  href!: string;
 
-  constructor(
-    private renderer: Renderer2,
-    private store: Store,
-    private http: HttpClient,
-    @Inject(DOCUMENT) private document: Document,
-
-
-  ) {}
+  constructor() {}
 
   ngOnInit(): void {
-    this.href = this.document.location.href;
   }
 }
